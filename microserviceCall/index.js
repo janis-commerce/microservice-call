52256b4ff89ebc062e992e46ad54add354109fac
Gastón Pereyra <gaston.pereyra@fizzmod.com>
2019-05-23 15:55:57 -0300

    Change Folder name

--- microserviceCall/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const MicroServiceCallError = require('./microservice-call-error');
-const MicroServiceCall = require('./microservice-call');
-
-module.exports = {
-	MicroServiceCallError,
-	MicroServiceCall
-};
